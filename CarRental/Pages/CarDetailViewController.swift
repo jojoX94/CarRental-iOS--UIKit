956c55847e2363e8ad22c3e11139bd1623ca1d05
Madiapps <madiapps@Mac-mini-de-Madiapps.local>
2022-08-01 17:08:17 +0300

    CarDetailViewControl completed

--- CarRental/Pages/CarDetailViewController.swift
+++ CarRental/Pages/CarDetailViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class CarDetailViewController: VC {
 
+    @IBOutlet weak var bottomContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bottomContainer.clipsToBounds = true
+        bottomContainer.layer.cornerRadius = 60
+        bottomContainer.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
     }
 }
