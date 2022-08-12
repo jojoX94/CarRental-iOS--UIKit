16575fd39a9c67bd6f22e2409af29f197f8937fb
Tahina R <ramarolacontact@gmail.com>
2022-08-12 09:59:19 +0300

    Refactor code on HomeViewControllers

--- CarRental/Pages/HomeViewController.swift
+++ CarRental/Pages/HomeViewController.swift
@@ -31,7 +31,100 @@ class HomeViewController: VC, UICollectionViewDelegate, UICollectionViewDataSour
     var animateIsDone = false
     
     override func viewWillDisappear(_ animated: Bool) {
+//        removeAnimation()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initView()
+        loadBrandsList()
+        loadCarList()
+    }
+    
+    fileprivate func initView() {
+        searchBar.searchTextField.backgroundColor = .clear
+        self.dismissKeyboard()
+    }
+    
+    func loadBrandsList() {
+        collectionView.register(BrandCollectionViewCell.nib(), forCellWithReuseIdentifier: BrandCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsHorizontalScrollIndicator = false
+    }
+    
+    func loadCarList() {
+        tableView.register(CarTableViewCell.nib(), forCellReuseIdentifier: CarTableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.showsVerticalScrollIndicator = false
+    }
+ 
+    /*
+        Collection view => Brand'lists
+     */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let model = Brands[indexPath.item]
+        let cv = collectionView.dequeueReusableCell(withReuseIdentifier: BrandCollectionViewCell.identifier, for: indexPath) as! BrandCollectionViewCell
+        switch indexPath.item {
+        case 1: cv.configure(imageName: "lambo.png")
+        case 2: cv.configure(imageName: "bmw.png")
+        case 3: cv.configure(imageName: "benz.png")
+        default:
+            cv.configure(imageName: "porshe.png")
+        }
+//        cv.configure(imageName: model.imageName)
+        return cv
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        tableViewTitle.isHidden = true
+        collectionTitle.isHidden = true
+        animateView()
+    }
+
+    
+    /*
+        End Of Collection view
+        Table view => Car'lists
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CarTableViewCell.identifier, for: indexPath) as! CarTableViewCell
+        cell.priceLabel.text = "$14.45"
+        cell.typeLabel.text = "X7"
+        cell.brandLabel.text = "BMW"
+        cell.img.image = UIImage(named: "q7.png")
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 260.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "CarDetailViewController") as? CarDetailViewController {
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+
+    /*
+        End of tableView
+        Function private :)
+     */
+    
+    // Remove Animation after view disappear
+    fileprivate func removeAnimation() {
         if animateIsDone {
             tableViewTitle.isHidden = false
             collectionTitle.isHidden = false
@@ -60,56 +153,9 @@ class HomeViewController: VC, UICollectionViewDelegate, UICollectionViewDataSour
             }
             animateIsDone = false
         }
-        
-        
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        collectionView.isPagingEnabled = false
-        initView()
-        loadBrandsList()
-        loadCarList()
     }
     
-    fileprivate func initView() {
-        searchBar.searchTextField.backgroundColor = .clear
-        self.dismissKeyboard()
-    }
-    
-    func loadBrandsList() {
-        collectionView.register(BrandCollectionViewCell.nib(), forCellWithReuseIdentifier: BrandCollectionViewCell.identifier)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.showsHorizontalScrollIndicator = false
-    }
-    
-    func loadCarList() {
-        tableView.register(CarTableViewCell.nib(), forCellReuseIdentifier: CarTableViewCell.identifier)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.showsVerticalScrollIndicator = false
-    }
- 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let model = Brands[indexPath.item]
-        let cv = collectionView.dequeueReusableCell(withReuseIdentifier: BrandCollectionViewCell.identifier, for: indexPath) as! BrandCollectionViewCell
-        switch indexPath.item {
-        case 1: cv.configure(imageName: "lambo.png")
-        case 2: cv.configure(imageName: "bmw.png")
-        case 3: cv.configure(imageName: "benz.png")
-        default:
-            cv.configure(imageName: "porshe.png")
-        }
-//        cv.configure(imageName: model.imageName)
-        return cv
-    }
-    
+    // Animate on focus on textfield or click on branch'view
     fileprivate func animateView() {
         if !animateIsDone {
             NSLayoutConstraint.deactivate([
@@ -126,54 +172,11 @@ class HomeViewController: VC, UICollectionViewDelegate, UICollectionViewDataSour
             ])
             UIView.animate(withDuration: 0.3) {
                 self.view.layoutIfNeeded()
-//                self.TopContainer.backgroundColor = .clear
+                self.TopContainer.backgroundColor = .clear
             }
             animateIsDone = true
         }
     }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        tableViewTitle.isHidden = true
-        collectionTitle.isHidden = true
-        animateView()
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CarTableViewCell.identifier, for: indexPath) as! CarTableViewCell
-        cell.priceLabel.text = "$14.45"
-        cell.typeLabel.text = "X7"
-        cell.brandLabel.text = "BMW"
-        cell.img.image = UIImage(named: "q7.png")
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 260.0
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "CarDetailViewController") as? CarDetailViewController {
-            navigationController?.pushViewController(vc, animated: true)
-        }
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 
