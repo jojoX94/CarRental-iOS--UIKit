d1b0d9747db1835f107adf7b803f22cd75c0db00
Tahina R <ramarolacontact@gmail.com>
2022-08-19 10:41:27 +0300

    Update view on CarDetailsVC

--- CarRental/Pages/CarDetailViewController.swift
+++ CarRental/Pages/CarDetailViewController.swift
@@ -13,18 +13,23 @@ class CarDetailViewController: VC {
 
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
     @IBOutlet weak var bottomContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initView()
+    }
+    
+    fileprivate func initView() {
         if let data = model {
             img.loadImageUsingCache(withUrl: data.car_image)
+            name.text = data.car_brand.uppercased() + " " + data.car_model.uppercased()
+            price.text = "$" + data.rent_prize + " per days"
         }
         
         bottomContainer.clipsToBounds = true
         bottomContainer.layer.cornerRadius = 60
         bottomContainer.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
     }
-    
-    
 }
