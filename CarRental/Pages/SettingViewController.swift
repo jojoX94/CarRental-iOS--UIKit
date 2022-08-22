c79e34f25ad2c8d72348b81eaf6f58fc153dfddd
Tahina R <ramarolacontact@gmail.com>
2022-08-22 17:49:35 +0300

    SettingScreen init

--- CarRental/Pages/SettingViewController.swift
+++ CarRental/Pages/SettingViewController.swift
@@ -6,23 +6,43 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SettingViewController: VC {
 
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var authButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        profileImageView.makeRounded()
-        // Do any additional setup after loading the view.
+        initView()
     }
     
+    fileprivate func initView() {
+        profileImageView.makeRounded()
+        Auth.auth().addStateDidChangeListener {
+            [weak self] auth , user in
+            if let user = user {
+                self?.authButton.backgroundColor = .systemGreen
+                self?.authButton.setTitle("Sign Out", for: .normal)
+            } else {
+                self?.authButton.backgroundColor = UIColor(named: "regularPink")
+                self?.authButton.setTitle("Sign Up", for: .normal) 
+            }
+        }
+    }
 
     @IBAction func goToSignInScreen(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let signUpViewController = storyboard.instantiateViewController(identifier: "SignUpViewController")
-        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(signUpViewController)
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//            let signUpViewController = storyboard.instantiateViewController(identifier: "SignUpViewController")
+//        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(signUpViewController)
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
     }
     
     /*
